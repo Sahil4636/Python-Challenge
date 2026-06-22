6905f3f1c623c52669a6d71a4985e7a01771d6e9
Sahil4636 <sahilbdhanvijclg@gmail.com>
2026-06-23 00:05:25 +0530

    Hello World

---
+++ Hello World.py
@@ -0,0 +1,19 @@
+print("Hello World")
+
+print(2+4)
+print(4-3)
+print(2*3)
+print(4/2)
+print(3**2) #exponetial
+print(3%2)  #modulus
+print(3//2) # floor division
+
+# Cheking data types
+print(type(10)) # Int
+print(type(3.14)) # Float
+print(type(1+5j)) # complex 
+print(type("Hello")) # string
+print(type([1,2,3])) # list
+print(type({'name':"Sahil"})) # dictionary
+print(type({9.8, 3.14, 2.7})) # tuple
+print(type(True)) # Boolean
