3624d5edcd276aa47abc53214d8fba84c3f133fe
Sahil4636 <sahilbdhanvijclg@gmail.com>
2026-07-07 22:20:05 +0530

    Day13

--- Day12sysmodule.py
+++ Day12sysmodule.py
@@ -1,25 +1,19 @@
-import sys
-
-def function1():
-    print("First function")
-
-def function2():
-    print("Second function")
-
-arguments = sys.argv[1:]
-
-if len(arguments) != 1:
-    print(f'ERROR: 1 argument expected, {len(arguments)} provided')
-    sys.exit(1)
-
-argument = arguments[0]
-if argument == 'f' or argument == 'first':
-    function1()
-elif argument == 's' or argument == 'second':
-    function2()
-else:
-    print(f'ERROR: Invalid argument {argument}')
-    sys.exit(1)
-
-
-
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "28d17ad5",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "language_info": {
+   "name": "python"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
